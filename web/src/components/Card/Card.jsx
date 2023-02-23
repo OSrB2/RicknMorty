8e7c664fd7852b4a32988f62421e54e45ffa80c8
Pedro <pedro_oliva@oulook.com.br>
2023-02-23 13:27:24 -0300

    feat: create tag field

--- web/src/components/Card/Card.jsx
+++ web/src/components/Card/Card.jsx
@@ -2,10 +2,23 @@ import './Card.css';
 
 function Card(props) {
   const item = props.item;
+  const tags = item.tag;
+
   return (
-    <div className="card">
+    <div className='card'>
       <h1>{item.name}</h1>
       <img src={item.imageUrl} alt={item.name} title={item.name} />
+      {tags && (
+        <div className='tag-wrapper'>
+          {tags.map((tag, index) => {
+            return (
+              <div key={`tag-${index}`} className='tag'>
+                {tag}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
